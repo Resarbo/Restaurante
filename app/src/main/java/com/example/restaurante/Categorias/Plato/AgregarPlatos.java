bba4df7d844373dc60b670c8ad9365f19a2ff4bc
LuisBarrial <angeloxz.com@gmail.com>
2023-07-18 23:14:28 -0500

    modificando el text view adapter e intent from blob update to our database

--- app/src/main/java/com/example/restaurante/Categorias/Plato/AgregarPlatos.java
+++ app/src/main/java/com/example/restaurante/Categorias/Plato/AgregarPlatos.java
@@ -4,16 +4,22 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.restaurante.Conexion;
 import com.example.restaurante.R;
-import com.squareup.picasso.Picasso;
+import com.mysql.jdbc.PreparedStatement;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -22,7 +28,9 @@ public class AgregarPlatos extends AppCompatActivity {
     TextView NombrePlato, CantidadPlato, DescripcionPlato, PrecioPlato;
     Button AgregarPlato;
 
-    int estado=0, idPlato;
+    ImageView imagenAgregarPlato;
+
+    private Uri imagenSeleccionadaUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,7 @@ public class AgregarPlatos extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
 
+
         NombrePlato = findViewById(R.id.NombrePlato);
         PrecioPlato = findViewById(R.id.PrecioPlato);
         CantidadPlato = findViewById(R.id.CantidadPlato);
@@ -41,65 +50,39 @@ public class AgregarPlatos extends AppCompatActivity {
 
         AgregarPlato = findViewById(R.id.AgregarPlato);
 
-        Bundle intent = getIntent().getExtras();
-        if(intent != null){
-            //setear
-            NombrePlato.setText(intent.getString("nombre"));
-            PrecioPlato.setText(intent.getString("precio"));
-            CantidadPlato.setText(intent.getString("cantidad"));
-            DescripcionPlato.setText(intent.getString("descripcion"));
-            idPlato = Integer.parseInt(intent.getString("idPlato"));
-
-            actionBar.setTitle("Actualizar");
-            String actualizar = "Actualizar";
-            AgregarPlato.setText(actualizar);
-        }
+        imagenAgregarPlato = findViewById(R.id.ImagenAgregarPlato);
 
         AgregarPlato.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (AgregarPlato.getText().equals("Agregar")){
-                    SubirPlato();
-                } else if (AgregarPlato.getText().equals("Actualizar")) {
-                    ActualizarPlato();
-                }
+                SubirPlato();
+            }
+        });
+
+        imagenAgregarPlato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ObtenerImg();
             }
         });
     }
 
     private void SubirPlato() {
+        int id = 0;
         Connection connection = Conexion.connectionclass();
         try{
+            InputStream inputStream = getContentResolver().openInputStream(imagenSeleccionadaUri);
+            byte[] imagenBytes = convertInputStreamToByteArray(inputStream);
+            inputStream.close();
             if(connection!= null){
-                String query =  "Insert into Platos values ('"
-                        + NombrePlato.getText().toString() + "','"
-                        + PrecioPlato.getText().toString() + "','"
-                        + CantidadPlato.getText().toString() + "','"
-                        + DescripcionPlato.getText().toString() + "','"
-                        + estado + "')";
-                Statement st = connection.createStatement();
-                ResultSet rs = st.executeQuery(query);
-            }
-        }catch (Exception e){
-            Log.e("error",e.getMessage());
-        }
-        startActivity(new Intent(AgregarPlatos.this, PlatosA.class));
-        finish();
-    }
-
-    public void ActualizarPlato(){
-        Connection connection = Conexion.connectionclass();
-        try{
-            if(connection!= null){
-                String query =  "update Platos set " +
-                        "precio='"+PrecioPlato.getText().toString()+"', " +
-                        "nombre='"+NombrePlato.getText().toString()+"', " +
-                        "cantidad='"+CantidadPlato.getText().toString()+"'," +
-                        "descripcion='"+DescripcionPlato.getText().toString()+"', " +
-                        "estado='"+1+"' where " +
-                        "id_plato = '"+idPlato+"'";
-                Statement st = connection.createStatement();
-                ResultSet rs = st.executeQuery(query);
+                String query = "INSERT INTO Platos (nombre, precio, cantidad, descripcion, imagen_blob) VALUES (?, ?, ?, ?, ?)";
+                PreparedStatement statement = (PreparedStatement) connection.prepareStatement(query);
+                statement.setString(1, NombrePlato.getText().toString());
+                statement.setString(2, PrecioPlato.getText().toString());
+                statement.setString(3, CantidadPlato.getText().toString());
+                statement.setString(4, DescripcionPlato.getText().toString());
+                statement.setBytes(5, imagenBytes);
+                statement.executeUpdate();
             }
         }catch (Exception e){
             Log.e("error",e.getMessage());
@@ -112,5 +95,36 @@ public class AgregarPlatos extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return super.onSupportNavigateUp();
+    }
+
+    public void ObtenerImg() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, 100);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            imagenSeleccionadaUri = data.getData();
+
+            if (imagenSeleccionadaUri != null) {
+                imagenAgregarPlato.setImageURI(imagenSeleccionadaUri);
+            }
+        }
+    }
+
+    private byte[] convertInputStreamToByteArray(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[1024];
+        while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+        buffer.flush();
+        return buffer.toByteArray();
     }
 }
