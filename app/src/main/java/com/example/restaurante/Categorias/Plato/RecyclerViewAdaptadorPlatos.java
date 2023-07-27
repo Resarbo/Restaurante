2e031bcec6c53e7bc8982eb6b38f0e6b8b5a41d3
LuisBarrial <angeloxz.com@gmail.com>
2023-07-27 15:49:40 -0500

    modificacion ultima

--- app/src/main/java/com/example/restaurante/Categorias/Plato/RecyclerViewAdaptadorPlatos.java
+++ app/src/main/java/com/example/restaurante/Categorias/Plato/RecyclerViewAdaptadorPlatos.java
@@ -1,36 +1,39 @@
 package com.example.restaurante.Categorias.Plato;
 
-import android.content.Context;
-import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.restaurante.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class RecyclerViewAdaptadorPlatos extends RecyclerView.Adapter<RecyclerViewAdaptadorPlatos.ViewHolder> {
-    private OnClickListener onClickListener;
-    public List<Plato> cocineroPlato;
     public static class ViewHolder extends RecyclerView.ViewHolder{
-        Context context;
         private TextView NombreTXTPlato, PrecioTXTPlato, IdTXTPlato,CantidadTXTPlato,DescripcionTXTPlato;
+        private ImageView image;
 
         public ViewHolder(View view){
             super(view);
-            context = view.getContext();
             NombreTXTPlato = (TextView)view.findViewById(R.id.NombreTXTPlato);
             IdTXTPlato = (TextView)view.findViewById(R.id.IdTXTPlato);
             PrecioTXTPlato = (TextView)view.findViewById(R.id.PrecioTXTPlato);
             CantidadTXTPlato = (TextView)view.findViewById(R.id.CantidadTXTPlato);
             DescripcionTXTPlato = (TextView)view.findViewById(R.id.DescripcionTXTPlato);
+            image = view.findViewById(R.id.ImagenverPlato);
         }
     }
+
+    public List<Plato> cocineroPlato;
 
     public RecyclerViewAdaptadorPlatos(List<Plato> cocineroPlato){
         this.cocineroPlato = cocineroPlato;
@@ -45,38 +48,22 @@ public class RecyclerViewAdaptadorPlatos extends RecyclerView.Adapter<RecyclerVi
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Plato item = cocineroPlato.get(position);
         holder.NombreTXTPlato.setText(cocineroPlato.get(position).getNombre());
         holder.DescripcionTXTPlato.setText(cocineroPlato.get(position).getDescripcion());
-        holder.PrecioTXTPlato.setText(String.valueOf(cocineroPlato.get(position).getPrecio()));
-        holder.IdTXTPlato.setText(String.valueOf(cocineroPlato.get(position).getId()));
-        holder.CantidadTXTPlato.setText(String.valueOf(cocineroPlato.get(position).getCantidad()));
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (onClickListener != null) {
-                    onClickListener.onClick(holder.getAdapterPosition(), item);
-                    Intent intent = new Intent(holder.context, PlatoDetalle.class);
-                    intent.putExtra("idPlato",holder.IdTXTPlato.getText());
-                    holder.context.startActivity(intent);
-                }
-            }
-        });
+        holder.PrecioTXTPlato.setText("S/" + String.valueOf(cocineroPlato.get(position).getPrecio()));
+        holder.IdTXTPlato.setText(null);
+        holder.CantidadTXTPlato.setText("disponible: "+String.valueOf(cocineroPlato.get(position).getCantidad()));
+        byte[] imagenBytes = cocineroPlato.get(position).getImagen();
+        Bitmap bitmapImagen = BitmapFactory.decodeByteArray(imagenBytes, 0, imagenBytes.length);
+        Bitmap result = Bitmap.createScaledBitmap(bitmapImagen, 200, 200, false);
+        holder.image.setImageBitmap(result);
+
     }
 
     @Override
     public int getItemCount() {
         return cocineroPlato.size();
     }
-
-    public void setOnClickListener(OnClickListener onClickListener) {
-        this.onClickListener = onClickListener;
-    }
-
-    public interface OnClickListener {
-        void onClick(int position, Plato model);
-    }
-
 
 
 }
